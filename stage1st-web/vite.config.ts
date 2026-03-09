15967454fe65de0142aa73b1747600ee1696292a
refer <73151182+referiw@users.noreply.github.com>
2026-03-10 01:47:21 +0800

    Fix vite.config.ts

--- stage1st-web/vite.config.ts
+++ stage1st-web/vite.config.ts
@@ -1,19 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [
     vue({
       template: { transformAssetUrls }
     }),
-    quasar({
-      sassVariables: 'src/css/quasar.variables.scss'
-    })
+    quasar()
   ],
   resolve: {
     alias: {
-      '@': '/src'
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    outDir: 'dist'
   }
 })
