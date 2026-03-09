b1d67b1841309a84cf7a693bb963e3486041e570
refer <73151182+referiw@users.noreply.github.com>
2026-03-10 01:59:34 +0800

    Simple router

--- stage1st-web/src/router.js
+++ stage1st-web/src/router.js
@@ -1,16 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
-  { path: '/', name: 'Home', component: () => import('../pages/IndexPage.vue') },
-  { path: '/report', name: 'Report', component: () => import('../pages/ReportPage.vue') },
-  { path: '/history', name: 'History', component: () => import('../pages/HistoryPage.vue') },
-  { path: '/about', name: 'About', component: () => import('../pages/AboutPage.vue') },
-  { path: '/:catchAll(.*)*', component: () => import('../pages/ErrorNotFound.vue') }
+  { path: '/', name: 'Home', component: () => import('./pages/IndexPage.vue') },
+  { path: '/:pathMatch(.*)*', component: () => import('./pages/IndexPage.vue') }
 ]
 
-const router = createRouter({
-  history: createWebHistory(),
-  routes
-})
-
-export default router
+export default createRouter({ history: createWebHistory(), routes })
