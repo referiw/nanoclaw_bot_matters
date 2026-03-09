5f4220ea29350dd444db11afa4657b71e367178e
refer <73151182+referiw@users.noreply.github.com>
2026-03-10 01:44:18 +0800

    Update router for Vite

--- stage1st-web/src/router/index.ts
+++ stage1st-web/src/router/index.ts
@@ -1,15 +1,10 @@
-import { route } from 'quasar/wrappers'
 import { createRouter, createWebHistory } from 'vue-router'
 import routes from './routes'
 
-export default route(function () {
-  const createHistory = createWebHistory
-  
-  const Router = createRouter({
-    scrollBehavior: () => ({ left: 0, top: 0 }),
-    routes,
-    history: createHistory(import.meta.env.BASE_URL)
-  })
-
-  return Router
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+  scrollBehavior: () => ({ left: 0, top: 0 })
 })
+
+export default router
