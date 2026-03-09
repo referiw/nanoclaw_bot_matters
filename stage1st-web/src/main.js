bdc132cd69476fb20c043e0165d7fcedf2f01e61
refer <73151182+referiw@users.noreply.github.com>
2026-03-10 01:59:25 +0800

    Simple main.js

--- stage1st-web/src/main.js
+++ stage1st-web/src/main.js
@@ -1,16 +1,6 @@
 import { createApp } from 'vue'
-import { Quasar, Notify, Loading, Dialog } from 'quasar'
-import quasarLang from 'quasar/lang/zh-CN.esm.prod'
-
 import App from './App.vue'
 import router from './router.js'
+import './css/app.css'
 
-import '@quasar/extras/roboto-font/roboto-font.css'
-import '@quasar/extras/material-icons/material-icons.css'
-import 'quasar/src/css/index.sass'
-import './css/app.scss'
-
-const app = createApp(App)
-app.use(router)
-app.use(Quasar, { plugins: { Notify, Loading, Dialog }, lang: quasarLang })
-app.mount('#app')
+createApp(App).use(router).mount('#app')
