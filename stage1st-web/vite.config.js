ab480bb95410be2fdb023bc11d72f302f5c96063
refer <73151182+referiw@users.noreply.github.com>
2026-03-10 01:59:23 +0800

    Simple vite config

--- stage1st-web/vite.config.js
+++ stage1st-web/vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
 export default defineConfig({
-  plugins: [
-    vue({ template: { transformAssetUrls } }),
-    quasar()
-  ]
+  plugins: [vue()]
 })
