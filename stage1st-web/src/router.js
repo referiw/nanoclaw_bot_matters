bf51a637ac983f24f0e66d0f53d542b50f1c7604
refer <73151182+referiw@users.noreply.github.com>
2026-03-10 13:33:34 +0800

    Update router to include ReportPage

--- stage1st-web/src/router.js
+++ stage1st-web/src/router.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   { path: '/', name: 'Home', component: () => import('./pages/IndexPage.vue') },
+  { path: '/report', name: 'Report', component: () => import('./pages/ReportPage.vue') },
   { path: '/:pathMatch(.*)*', component: () => import('./pages/IndexPage.vue') }
 ]
 
